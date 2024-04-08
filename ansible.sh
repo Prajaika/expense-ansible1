77c4e130a963ec70578b11bdea55dfef51665ddb
Preethi <preethi5014@gmail.com>
2024-04-08 08:19:50 +0800

    Update secrets playbook

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 component=$1
 
-ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.Prajaika.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
+ansible_playbook get-secrets.yml -e vault_token=$vault_token
+ansible-playbook -i $component-$env.Prajaika.online, -e env=$env -e role_name=$component expense.yml -e 'secrets.json'
+
 
